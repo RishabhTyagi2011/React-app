bfe9e345512894c43a28130b52802eae4279c78f
RishabhTyagi2011 <rishabh.tyagi.csit.2019@miet.ac.in>
2022-02-20 09:16:36 +0530

    Updates

---
+++ precache-manifest.1c2415bc0d543a400c12c267c368ef9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307fd63e3aebc259b4a04ad0215d42b5",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "75cac14b7d2f36f1f495",
+    "url": "/React-app/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "5ab0307a6d692c106f05",
+    "url": "/React-app/static/js/2.70863f92.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/React-app/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "75cac14b7d2f36f1f495",
+    "url": "/React-app/static/js/main.f53db396.chunk.js"
+  },
+  {
+    "revision": "0313fbf045e7704e7802",
+    "url": "/React-app/static/js/runtime~main.e7a4fdd6.js"
+  }
+]);
